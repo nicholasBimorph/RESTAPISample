13f92a5c95c20459f6a46b104abe5081100f3dbf
Nicholas Rawitscher <nicholas@bimorph.com>
2021-09-08 08:38:34 -0500

    dataNodeFactory

---
+++ HB.RestAPI.Core/Models/Factories/DataNodeFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.RestAPI.Core.Models.Factories
+{
+    /// <summary>
+    /// A class which encapsulates the
+    /// creation of <see cref="DataNode" />'s.
+    /// </summary>
+    public class DataNodeFactory
+    {
+        private readonly ISerializer _serializer;
+
+        /// <summary>
+        /// Construct a <see cref="DataNodeFactory" />.
+        /// </summary>
+        public DataNodeFactory(ISerializer serializer)
+        {
+            _serializer = serializer;
+        }
+
+        /// <summary>
+        /// Creates a <see cref="DataNode" /> from an
+        /// <see cref="EntityObject" />.
+        /// </summary>
+        public DataNode Create(IEntity entity)
+        {
+            string jsonEntity = _serializer.Serialize(entity);
+
+            return new DataNode(jsonEntity, entity.GetType(), entity.HostApplicationId);
+        }
+    }
+}
